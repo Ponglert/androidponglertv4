924c1b24d09c5a4a6c715905db81efb90fecd9cc
Ponglert <asakuratran@gmail.com>
2026-01-27 15:04:14 +0700

    upv10

--- app/src/main/java/com/ps/ponglertv3/SettingFragment.java
+++ app/src/main/java/com/ps/ponglertv3/SettingFragment.java
@@ -1,7 +1,11 @@
 package com.ps.ponglertv3;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -12,6 +16,7 @@ import android.widget.Button;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class SettingFragment extends Fragment {
 
@@ -40,5 +45,24 @@ public class SettingFragment extends Fragment {
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
         mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         return view;
+    }
+
+    private void Get_Current_location() {
+        if (ActivityCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getActivity(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            clat[0] = location.getLatitude();
+                            clng[0] = location.getLongitude();
+                        }
+                    }
+                });
     }
 }
