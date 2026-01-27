b5d862efe4d0b4b124a60c251492d6bdb1f32cdc
Ponglert <asakuratran@gmail.com>
2026-01-27 14:58:43 +0700

    upv9

--- app/src/main/java/com/ps/ponglertv3/SettingFragment.java
+++ app/src/main/java/com/ps/ponglertv3/SettingFragment.java
@@ -7,8 +7,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class SettingFragment extends Fragment {
+
+    private int locationRequestCode = 1000;
+    private Button button;
+    private SupportMapFragment mapFragment;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private double[] clat = {0};
+    private double[] clng = {0};
 
     public SettingFragment() {
         // Required empty public constructor
@@ -23,6 +35,10 @@ public class SettingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        View view =  inflater.inflate(R.layout.fragment_setting, container, false);
+        button = (Button) view.findViewById(R.id.button);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        return view;
     }
 }
